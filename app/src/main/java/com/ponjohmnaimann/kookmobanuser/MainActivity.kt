85783d379349be56b7719107b1df4f6a06007c8f
Jinwhuy <71877784+Jinwhuy@users.noreply.github.com>
2020-10-15 09:41:41 +0900

    New Project 생성

---
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ponjohmnaimann.kookmobanuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
