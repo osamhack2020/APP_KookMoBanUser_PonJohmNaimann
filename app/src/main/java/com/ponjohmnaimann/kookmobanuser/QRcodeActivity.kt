f6f444c146b3c4b87bafe71207f949d041cc2c7c
Jinwhuy <jinwhuy.lee@gmail.com>
2020-10-15 10:28:46 +0900

    [ENH] TOTP기능 구현
    Resolves #5

--- app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
@@ -8,13 +8,18 @@ import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import kotlinx.android.synthetic.main.activity_q_rcode.*
+import java.util.*
 
 class QRcodeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_q_rcode)
 
-        val qr_contents = "Hello World"
+        val seed ="4657489446416213 "
+
+        val time = Calendar.getInstance()
+        val steps = TOTP.calcSteps(time.timeInMillis / 1000, 0L, 10L)
+        val qr_contents = TOTP.generateTOTP(seed, steps, "8", "HMacSHA512")
 
         val multiFormatWriter = MultiFormatWriter()
         val bitMatrix: BitMatrix = multiFormatWriter.encode(qr_contents.toString(), BarcodeFormat.QR_CODE, 300, 300)
