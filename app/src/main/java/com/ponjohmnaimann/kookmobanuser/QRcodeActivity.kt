5b7423913acf3f26e7490923d795821786f271b3
Jinwhuy <wls7925980!>
2020-10-25 12:56:08 +0900

    [ENH] TOTP값 표시 토스트 메시지 추가
    * 파라미터 이름을 가독성이 뛰어나도록 수정
    * QR코드 생성시 토스트 메시지 추가

--- app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
@@ -22,22 +22,23 @@ class QRcodeActivity : AppCompatActivity() {
         val time = Calendar.getInstance()
         val steps = TOTP.calcSteps(time.timeInMillis / 1000, 0L, 10L)
 
-        val qrContent = TOTP.generateTOTP(PrefInit.prefs.seed, steps, "8", "HMacSHA512")
+        val tOTPValue = TOTP.generateTOTP(PrefInit.prefs.seed, steps, "8", "HMacSHA512")
 
         val qrParams = HashMap<String, String?>()
         qrParams["deviceID"] = PrefInit.prefs.deviceID
         qrParams["adminID"] = PrefInit.prefs.adminID
-        qrParams["TOTP"] = qrContent
+        qrParams["TOTP"] = tOTPValue
 
         val mapper = jacksonObjectMapper()
-        val jsonStr = mapper.writeValueAsString(qrParams)
+        val qrContent = mapper.writeValueAsString(qrParams)
 
         val multiFormatWriter = MultiFormatWriter()
-        val bitMatrix: BitMatrix = multiFormatWriter.encode(jsonStr, BarcodeFormat.QR_CODE, 300, 300)
+        val bitMatrix: BitMatrix = multiFormatWriter.encode(qrContent, BarcodeFormat.QR_CODE, 300, 300)
         val barcodeEncoder = BarcodeEncoder()
         val bitmap: Bitmap = barcodeEncoder.createBitmap(bitMatrix)
 
         qr_image.setImageBitmap(bitmap)
+        Toast.makeText(this, "TOTP : $tOTPValue", Toast.LENGTH_SHORT).show()
 
     }
 }
