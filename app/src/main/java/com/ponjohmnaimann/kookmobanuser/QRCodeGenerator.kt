10890ab6aab3506e5521f21066d088fdd6d62700
Jinwhuy <jinwhuy.lee@gmail.com>
2020-10-26 19:38:48 +0900

    [ENH] QR코드 암호화(Red색상)
    * getPixel, setPixel 사용시 속도가 너무 느리기 때문에 앱이 강제종료됨
    
    * getPixels로 속도를 높임

--- app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRCodeGenerator.kt
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRCodeGenerator.kt
@@ -3,15 +3,18 @@ package com.ponjohmnaimann.kookmobanuser
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.ColorSpace
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.graphics.red
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import kotlinx.coroutines.*
+import java.nio.IntBuffer
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -38,6 +41,32 @@ fun qrCodeGenerator(context : Context, view : View, deviceID: String?, adminID: 
     runBlocking {
         val qrEncoder = GlobalScope.launch(Dispatchers.Main) {
 
+            var pixelsArray = IntArray(bitmap.width * bitmap.height)
+            bitmap.getPixels(pixelsArray, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+            for (y in 0 until pixelsArray.size) {
+                if (pixelsArray[y] != -1) {
+
+                    val red = 255
+                    val green = Random().nextInt(256)
+                    val blue = Random().nextInt(256)
+                    val newColorForBlack = Color.rgb(red, green, blue)
+                    pixelsArray[y] = newColorForBlack
+
+                }
+                else {
+
+                    val red = 0
+                    val green = Random().nextInt(256)
+                    val blue = Random().nextInt(256)
+                    val newColorForWhite = Color.rgb(red, green, blue)
+                    pixelsArray[y] = newColorForWhite
+
+                }
+            }
+
+            qrImage.setImageBitmap(bitmap)
+            /*
             for (i in 0..bitmap.width) {
                 for (j in 0..bitmap.height) {
                     var red = Color.red(bitmap.getPixel(i, j))
@@ -48,12 +77,11 @@ fun qrCodeGenerator(context : Context, view : View, deviceID: String?, adminID: 
                     val rgb = Color.rgb(red, green, blue)
                     bitmap.setPixel(i, j, rgb)
                 }
-            }
+            }*/
 
         }
         qrEncoder.join()
     }
 
-    qrImage.setImageBitmap(bitmap)
 
 }
