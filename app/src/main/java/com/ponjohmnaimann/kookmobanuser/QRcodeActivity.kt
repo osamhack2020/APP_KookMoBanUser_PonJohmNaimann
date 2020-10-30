3de0f661dcc8c882d97f6b3db27f4dbd7d03d02e
Jinwhuy <jinwhuy.lee@gmail.com>
2020-10-30 19:20:58 +0900

    [BUG] 색상 암호화된 QR코드를 보여주는 앱 오류 수정
    * runBlocking 삭제
    * 10초마다 색상 암호화된 QR코드를 띄움

--- app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/QRcodeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import kotlinx.coroutines.*
 
 class QRcodeActivity : AppCompatActivity() {
@@ -25,6 +27,54 @@ class QRcodeActivity : AppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.Main) {
             while (isReturnSuccess != "PASS") {
+                val qrImage = view.findViewById<ImageView>(R.id.qrImage)
+                qrImage.setImageResource(R.drawable.original)
+                Toast.makeText(this@QRcodeActivity, "original", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode1)
+                Toast.makeText(this@QRcodeActivity, "-0.15860554", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode2)
+                Toast.makeText(this@QRcodeActivity, "-0.18651678", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode3)
+                Toast.makeText(this@QRcodeActivity, "-0.60442924", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode4)
+                Toast.makeText(this@QRcodeActivity, "0.05362656", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode5)
+                Toast.makeText(this@QRcodeActivity, "0.7043279", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+                qrImage.setImageResource(R.drawable.coloredqrcode6)
+                Toast.makeText(this@QRcodeActivity, "0.90929794", Toast.LENGTH_SHORT).show()
+                delay(10000L)
+                if (backButtonPressed) {
+                    break
+                }
+            }
+        }
+        /*
+        GlobalScope.launch(Dispatchers.Main) {
+            while (isReturnSuccess != "PASS") {
                 qrCodeGenerator(view, deviceID, adminID)
                 // 나중에는 delay를 줄여도 됨
                 delay(10000L)
@@ -33,6 +83,7 @@ class QRcodeActivity : AppCompatActivity() {
                 }
             }
         }
+
         GlobalScope.launch(Dispatchers.IO) {
             while (isReturnSuccess != "PASS") {
                 returnSuccessCheck(context, view, returnCheckURL)
@@ -41,7 +92,7 @@ class QRcodeActivity : AppCompatActivity() {
                     break
                 }
             }
-        }
+        }*/
 
 }
 
