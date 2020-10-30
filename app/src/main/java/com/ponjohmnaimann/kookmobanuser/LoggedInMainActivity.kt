922033879a7bd5f60c9c0070140e522f98470714
Jinwhuy <jinwhuy.lee@gmail.com>
2020-10-30 17:42:01 +0900

    [BUG, ENH] 앱 반납 페이지 오류 수정, license.md 와 readme.md 파일 수정

--- app/src/main/java/com/ponjohmnaimann/kookmobanuser/LoggedInMainActivity.kt
+++ app/src/main/java/com/ponjohmnaimann/kookmobanuser/LoggedInMainActivity.kt
@@ -3,7 +3,6 @@ package com.ponjohmnaimann.kookmobanuser
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_logged_in_main.*
 
@@ -15,8 +14,6 @@ class LoggedInMainActivity : AppCompatActivity() {
 
         return_btn.setOnClickListener {
 
-            val loadingText = findViewById<TextView>(R.id.loadingText)
-            loadingText.text = "QR코드 생성중"
             LoadingDialog(this).show()
 
             val qrIntent = Intent(this, QRcodeActivity::class.java)
@@ -26,17 +23,18 @@ class LoggedInMainActivity : AppCompatActivity() {
         }
     }
 
-    var backbtnPressedTime : Long = 0
+    private var backPressedTime : Long = 0
 
     override fun onBackPressed() {
+
         if (PrefInit.prefs.successLogIn == true) {
             val systemTime = System.currentTimeMillis()
-            val timeInterval = systemTime - backbtnPressedTime
+            val timeInterval = systemTime - backPressedTime
             if (timeInterval in 0..2000) {
                 moveTaskToBack(true)
             }
             else {
-                backbtnPressedTime = systemTime
+                backPressedTime = systemTime
                 Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
             }
         }
